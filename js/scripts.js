94173e353522fcbec6a040a9218621e3730cb0ad
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-27 18:06:48 +0200

    mod

--- js/scripts.js
+++ js/scripts.js
@@ -10,43 +10,56 @@
 
 
 // creo un alert dove mostrerò i numeri da ricordarsi
-alert("Indovina questi numeri: 1  2  3  4  5")
-const numeriUtente= (prompt("Inserisci i numeri appena visti"))
-const numeriDigitatiUtente = numeriUtente.split(" ")
-console.log(numeriDigitatiUtente)
+
+// const numeriUtente= prompt("Inserisci i numeri appena visti ")
+let numeriDaIndovinare = []
+while (numeriDaIndovinare.length < 5) {
+   let numero =  Math.round(Math.random() * 100)
+   numeriDaIndovinare.push(numero)
+}
+alert(`Indovina questi numeri: ${numeriDaIndovinare}`)
+
+
 // una volta chiuso l'alert darò 30 secondi all'utente per memorizzare i numeri
-// setTimeout(()=>{
-    
-//     },30000)
+setTimeout(()=>{
+    const numeriUtente= prompt("Inserisci i numeri appena visti separandoli da uno spazio")
+    const numeriDigitatiUtente = numeriUtente.split(" ")
+    confrontoNumeriUtente(numeriDigitatiUtente)
+    },2000)
+
 
 
 // creo un array con i numeri inseriti nell'alert
-const numeriDaIndovinare = [1,2,3,4,5]
+
+
+
+
 let arrayVuoto 
-// devo creare un array vuoto  e confrontarlo con l'array di numeri da indovinare se l'array resta vuoto signfica che l'utente non ha indovinato
+ 
+// devo creare un array vuoto e pushare all'interno di esso unicamente i numeri che l'utente ha digitato corrispondenti ai numeri da indovinare 
+// se l'array resta vuoto signfica che l'utente non ha indovinato
 // nessun numero se l'array vuoto ha un numero o due signfica che l'utente ha indovinato un x di numeri e tramite l'indice del mio array andrò a dire
 // quanti numeri ha indovinato
-function confrontoNumeriUtente(elementi) {
-  
+function confrontoNumeriUtente(numeriDigitatiUtente) {
+
     arrayVuoto=[]
 
-
     for (let x = 0; x < numeriDaIndovinare.length; x++) {
-        const element = numeriDaIndovinare[x];
-        console.log(element)
-    
-
-        if (numeriDigitatiUtente === numeriDaIndovinare[x]) {
-            arrayVuoto.push(numeriDigitatiUtente)
-            alert("bravo")
-            console.log(arrayVuoto)
-        }
+        const singoloNumero = numeriDaIndovinare[x];
+      
+        if (numeriDigitatiUtente.includes(singoloNumero.toString())) {
+            arrayVuoto.push(singoloNumero)
+        } 
     }
 
-    
-    
+    if (arrayVuoto.length > 0) {
+        alert(`Hai indovinato ${arrayVuoto.length} numeri`)    
+    }
+    else{
+        alert("Ritenta sarai più fortunato")
+    }
 
-    
+
+     console.log(arrayVuoto)
 }
 
-confrontoNumeriUtente(numeriDaIndovinare)
